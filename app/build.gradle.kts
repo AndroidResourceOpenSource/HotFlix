2d325feaaaabf6ea5c439f0fcfc21155fdf61fd3
vivek panchal <vivekpanchal64@gmail.com>
2023-05-28 13:58:00 +0530

    🔨 Updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,15 +4,15 @@ plugins {
 }
 
 android {
-    namespace = "com.androidresource.jetflix"
-    compileSdk = 33
+    namespace = Config.applicationId
+    compileSdk = Config.compileSdk
 
     defaultConfig {
-        applicationId = "com.androidresource.jetflix"
-        minSdk = 24
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        applicationId =Config.applicationId
+        minSdk = Config.minSdk
+        targetSdk = Config.targetSdk
+        versionCode = Config.versionCode
+        versionName = Config.versionName
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
@@ -51,19 +51,19 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.2")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    Deps.Androidx.androidx.forEach(::implementation)
+    implementation(platform(Deps.compose.composeBom))
+    Deps.compose.composeBomDeps.forEach(::implementation)
+   // implementation(Deps.Androidx.androidx)
+
+
+    //*********test Library ************
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    androidTestImplementation(platform(Deps.compose.composeBom))
+    Deps.testLibs.composeAndroidTests.forEach(::androidTestImplementation)
+    Deps.testLibs.composeDebugTests.forEach(::debugImplementation)
+//    androidTestImplementation(Deps.testLibs.composeAndroidTests)
+//    debugImplementation(Deps.testLibs.composeDebugTests)
 }
